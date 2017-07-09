137918d7e10bd359be8a0bc5264fa7e0227853cb
Jesse Benson <benson.jesse@gmail.com>
2017-07-08 21:36:46 -0700

    Add script to restart StatelessSvc.

---
+++ scripts/misc/Restart-StatelessSvc.ps1
@@ -0,0 +1,10 @@
+﻿Connect-ServiceFabricCluster azure-performance.jessebenson.me:19000 `
+    -X509Credential `
+    -ServerCertThumbprint "41AEC7DD46696FA88DAC6A5878F92E90C727726F" `
+    -FindType "FindByThumbprint" `
+    -FindValue "41AEC7DD46696FA88DAC6A5878F92E90C727726F" `
+    -StoreLocation "CurrentUser" `
+    -StoreName "My"
+
+Get-ServiceFabricApplication "fabric:/Latency.App" | Get-ServiceFabricService -ServiceName "fabric:/Latency.App/StatelessSvc" | Get-ServiceFabricPartition | Get-ServiceFabricReplica | Remove-ServiceFabricReplica -ForceRemove
+
